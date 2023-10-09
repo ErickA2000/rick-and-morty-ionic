c7a074f8fa713d1ec46a8583c9a813daa598b04a
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-10-09 17:47:15 -0500

    creacion de pagina, servicio e interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'character',
+    loadChildren: () => import('./character/character.module').then( m => m.CharacterPageModule)
+  },
 ];
 
 @NgModule({
